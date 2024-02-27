04e70b2b9bd2b02d104389002c1d35ffd8b608b7
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-02-27 15:43:15 +0100

    REMOVED: testing commented code

--- src/i18n.ts
+++ src/i18n.ts
@@ -11,33 +11,4 @@ i18n
     fallbackLng: 'en',
   });
 export default i18n;
-
-/*import i18n from "i18next";
-import i18nBackend from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
-
-export const LANGUAGES = [
-    { label: "Spanish", code: "es" },
-    { label: "English", code: "en" },
-];
-  
-const getCurrentHost =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5173"
-    : "https://oreplay.es";
-
-i18n
-  .use(i18nBackend)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: "en",
-    lng: "en",
-    interpolation: {
-      escapeValue: false,
-    },
-    backend: {
-      loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
-    },
-  });
-
-export default i18n; */
+// Bug with LanguageDetector. I guess it returns es-ES or similar and falls back kto english.
