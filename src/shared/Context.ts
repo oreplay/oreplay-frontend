2de1d1fe32a1440fee1ff906e438ae113fec3b68
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-07-02 12:35:03 +0200

    Move useRunners into context

--- src/shared/Context.ts
+++ src/shared/Context.ts
@@ -1,4 +1,5 @@
-import {ClassModel} from "./EntityTypes.ts";
+import {ClassModel, RunnerModel} from "./EntityTypes.ts";
 import {createContext} from "react";
 
 export const activeClassContext =  createContext<ClassModel|null>(null);
+export const runnerListContext = createContext<[RunnerModel[]|null,boolean]>([null,false]);
