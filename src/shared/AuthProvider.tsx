b314c2277fe12c0545ab6ebb7f29040e1c0f2286
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-07-03 21:02:54 +0200

    BUG fix in loading dashboard

--- src/shared/AuthProvider.tsx
+++ src/shared/AuthProvider.tsx
@@ -15,20 +15,19 @@ export function AuthProvider (props:{children: React.ReactNode}) {
   const [token,setToken] = useState<string | null>(null);
   const [user,setUser] = useState<UserModel | null>(null);
 
-  const loginAction = (user:string,password:string) => {
-    const signInPromise = validateSignIn(user,password)
-    return signInPromise.then((response)=>{
-        setToken(response.data.access_token)
-
-        getUserData(response.data.access_token).then((response)=>{
-          setUser(response.data)
-          console.log(response.data)
-        },(reason)=>{
-          console.log('Failed getUserData',reason)
-        })
-      }, (reason)=>{
-        console.log('Failed validateSignIn',reason)
-      })
+  const loginAction = async (user:string,password:string) => {
+    try {
+      const responseSignInToken = await validateSignIn(user,password)
+      setToken(responseSignInToken.data.access_token)
+      try {
+        const responseUserData = await getUserData(responseSignInToken.data.access_token)
+        setUser(responseUserData.data)
+      } catch (error) {
+        console.log('Failed getUserData',error)
+      }
+    } catch (error) {
+      console.log('signIn error',error)
+    }
   }
 
   const logoutAction = () => {
@@ -37,9 +36,9 @@ export function AuthProvider (props:{children: React.ReactNode}) {
     setUser(null)
   }
 
-
   return (
     <AuthContext.Provider value={{token,user,loginAction,logoutAction}}>
       {props.children}
-    </AuthContext.Provider>);
+    </AuthContext.Provider>
+  )
 }
