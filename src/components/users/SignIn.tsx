78579571f5e0a15536599ccaacba2067142c2453
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-07-05 22:20:38 +0200

    validate required field are filled before submit

--- src/components/users/SignIn.tsx
+++ src/components/users/SignIn.tsx
@@ -52,7 +52,7 @@ export default function SignIn(){
           <Typography component="h1" variant="h5">
             {t('Sign in.Sign in')}
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
