238d070e78fa328487f9bde3b8479cff6e7f9afc
Adrian <freefri@freefri.es>
2024-09-03 18:59:47 +0200

    #71 RuPWfSC4 quickly display token without the need to open the console (this UI should be improved)

--- src/components/administration/EventTokenDataGrid.tsx
+++ src/components/administration/EventTokenDataGrid.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {useAuth} from "../../shared/hooks.ts";
 import Button from "@mui/material/Button";
 import {invalidateEventToken, postEventToken} from "../../services/EventService.ts";
@@ -8,25 +9,29 @@ interface Props {
 
 export default function EventTokenDataGrid( props:Props ) {
   const {token} = useAuth()
+  const [text, setText] = useState<string>('')
 
   return (
-    <>
+    <div>
+      <div><b>{text}</b></div>
       <Button
-        onClick={()=>{
-          console.log( postEventToken(props.event_id,token as string) )
+        onClick={async ()=>{
+          const res = await postEventToken(props.event_id,token as string)
+          const newText = 'Copy your token here (it will not be displayed again): ' + res.data.token
+          setText(newText)
+          console.log(newText) // TODO remove console log and improve UI
         }
         }
       >
         Create Event Token
       </Button>
       <Button
-        onClick={()=>{
-          console.log( invalidateEventToken(props.event_id,"",token as string) )
+        onClick={async ()=>{
+          console.log( await invalidateEventToken(props.event_id,"",token as string) )
         }}
       >
         Delete Event Token (paste token in code)
       </Button>
-    </>
-
+    </div>
   )
 }
