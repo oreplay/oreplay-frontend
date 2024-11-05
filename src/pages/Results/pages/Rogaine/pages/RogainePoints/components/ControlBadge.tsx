7e7a373b557772ab030bbdbe6646e3eb6f2c6980
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-11-05 17:47:22 +0100

    ADD Rogaine points screen

---
+++ src/pages/Results/pages/Rogaine/pages/RogainePoints/components/ControlBadge.tsx
@@ -0,0 +1,30 @@
+import {Box} from "@mui/material";
+import React from "react";
+
+interface ControlBadgeProps {
+  number: bigint | number;
+  punched?: boolean;
+}
+
+const ControlBadge: React.FC<ControlBadgeProps> = ({ number, punched = false }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 40,           // Circle diameter
+        height: 40,          // Circle diameter
+        borderRadius: '50%', // Makes it a circle
+        backgroundColor: punched ? 'green' : 'gray',
+        color: 'white',
+        fontSize: '16px',
+        fontWeight: 'bold',
+      }}
+    >
+      {number.toString()}
+    </Box>
+  );
+};
+
+export default ControlBadge;
