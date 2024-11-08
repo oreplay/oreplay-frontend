65dcb4d0fe74b31ed600e27cbfcfe6315694cc9c
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-11-08 18:34:38 +0100

    Split down EventsList into several components and better styling

---
+++ src/pages/Results/pages/EventList/components/TodayEventButton.tsx
@@ -0,0 +1,51 @@
+import {Typography} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import {EventModel} from "../../../../../shared/EntityTypes.ts";
+import Button from "@mui/material/Button";
+
+interface TodayEventButtonProps {
+  event: EventModel;
+  index: number
+}
+
+export default function TodayEventButton(props:TodayEventButtonProps) {
+  const navigate = useNavigate();
+
+  const styles = {
+    typographyBox: {
+      color: "white",
+      margin: "12px",
+      textAlign: "left",
+      textTransform: 'none'
+    }
+  }
+
+  return (
+    <Button
+      key={props.event.id}
+      sx={{
+        borderRadius:"6px",
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"space-between",
+        alignItems:'flex-start',
+        backgroundColor: props.index%2 == 0 ? "primary.main": "secondary.main",
+        minWidth:"220px",
+        width:"220px",
+        height:"160px",
+        marginRight:"24px",
+        transition: "transform 0.2s, box-shadow 0.2s, background-color 0.2s",
+        transformOrigin: "center",  // Scale from the center without shifting position
+        "&:hover": {
+          backgroundColor: props.index % 2 === 0 ? "primary.main" : "secondary.main", //keep the color
+          transform: "scale(1.05)",  // Slightly scale up on hover
+        },
+      }}
+      onClick={() => navigate(`/competitions/${props.event.id}`)}
+    >
+      <Typography sx={styles.typographyBox} fontWeight={"bolder"}>
+        {props.event.description}
+      </Typography>
+    </Button>
+  )
+}
---
+++ src/pages/Results/pages/EventList/components/TodayEvents.tsx
@@ -0,0 +1,46 @@
+import {EventModel} from "../../../../../shared/EntityTypes.ts";
+import {useTranslation} from "react-i18next";
+import {Box, Typography} from "@mui/material";
+import TodayEventButton from "./TodayEventButton.tsx";
+
+interface TodayEventsProps {
+  eventList: EventModel[]
+}
+
+export default function TodayEvents(props:TodayEventsProps) {
+  const {t} = useTranslation();
+
+  if (props.eventList.length > 0) {
+    return (
+      <>
+        {/** Live today message **/}
+        <Box width={"100%"} display={"flex"} alignItems={"center"}>
+          <Box height={"16px"} width={"16px"} borderRadius={"50%"} bgcolor={"orange"}></Box>
+          <Box marginLeft={"12px"}>
+            <Typography>{t('EventList.LiveToday')}</Typography>
+          </Box>
+        </Box>
+
+        {/** Event list **/}
+        <Box
+          sx={{
+            overflowX: "auto",
+            padding: "12px"
+          }}
+          width={"100%"}
+          minHeight={"160px"}
+          marginTop={"24px"}
+          marginBottom={"24px"}
+          display={"flex"}
+        >
+          {
+            props.eventList.map((event, index) =>
+              <TodayEventButton key={event.id} event={event} index={index} /> )
+          }
+        </Box>
+      </>
+    )
+  } else {
+    return (<></>)
+  }
+}
