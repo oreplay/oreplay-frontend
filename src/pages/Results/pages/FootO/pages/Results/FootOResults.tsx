d0c9014605baad25cff013f2378b61eb032e2a50
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-11-14 23:12:49 +0100

    Display runner status in FootOResults

--- src/pages/Results/pages/FootO/pages/Results/FootOResults.tsx
+++ src/pages/Results/pages/FootO/pages/Results/FootOResults.tsx
@@ -6,6 +6,7 @@ import {RunnerModel} from "../../../../../../shared/EntityTypes.ts";
 import ResultListItem from "../../components/ResultListItem.tsx";
 import {Box, Typography} from "@mui/material";
 import {parseSecondsToMMSS, parseStartTime} from "../../../../../../shared/Functions.tsx";
+import {parseResultStatus} from "../../../../shared/functions.ts";
 
 export default function FootOResults() {
   const {t} = useTranslation();
@@ -19,76 +20,89 @@ export default function FootOResults() {
     return (
       <ResultListContainer>
         {
-          runnersList.map((runner: RunnerModel) => (
-            <ResultListItem
-              key={runner.id}
-            >
-              <Box
-                sx={{
-                  flexShrink: 0,
-                  display: "flex",          // Enables flex properties
-                  flexDirection: "column",  // Stack content vertically
-                  justifyContent: "flex-start",  // Align content to the top
-                  flexGrow: 1,
-                }}
+          runnersList.map((runner: RunnerModel) => {
+            const status = parseResultStatus(runner.runner_results[0].status_code as string)
+
+            return (
+              <ResultListItem
+                key={runner.id}
               >
-                <Typography sx={{color:'primary.main'}}>
-                  {`${runner.runner_results[0].position}.`}
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display:'flex',
-                  flexShrink: 1,
-                  flexDirection: 'column',
-                  justifyContent: 'space-between',
-                  width:'100%',
-                  marginLeft:'1em'
-                }}
-              >
-                <Box>
-                  <Typography>
-                    {`${runner.first_name} ${runner.last_name}`}
-                  </Typography>
-                  <Typography
-                    sx={{
-                      color:'text.secondary'
-                    }}
-                  >
-                    {`${runner.club.short_name}`}
+                <Box
+                  sx={{
+                    flexShrink: 0,
+                    display: "flex",          // Enables flex properties
+                    flexDirection: "column",  // Stack content vertically
+                    justifyContent: "flex-start",  // Align content to the top
+                    flexGrow: 1,
+                    width:'5px'
+                  }}
+                >
+                  <Typography sx={{color:'primary.main'}}>
+                    {runner.runner_results[0].position ? `${runner.runner_results[0].position}.` : ''}
                   </Typography>
                 </Box>
                 <Box
                   sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: "space-between",
-                    width: '100%'
+                    display:'flex',
+                    flexShrink: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    width:'100%',
+                    marginLeft:'1em'
                   }}
                 >
-                  <Box sx={{
-                    display:'inline-flex',
-                    flexDirection:'row',
-                    justifyContent:'space-between',
-                    gap:'.3em'
-                  }}>
-                    <Typography sx={{color:'secondary.main'}}>{`${t('ResultsStage.StartTime')}:`}</Typography>
-                    <Typography>{parseStartTime(runner.runner_results[0].start_time)}</Typography>
+                  <Box>
+                    <Typography>
+                      {`${runner.first_name} ${runner.last_name}`}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        color:'text.secondary'
+                      }}
+                    >
+                      {`${runner.club.short_name}`}
+                    </Typography>
                   </Box>
-                  <Box sx={{
-                    display:'inline-flex',
-                    flexDirection:'row',
-                    justifyContent:'space-between',
-                    gap:'.3em'
-                  }}>
-                    <Typography sx={{color:'secondary.main'}}>{`${t('ResultsStage.FinishTime')}:`}</Typography>
-                    <Typography>{parseSecondsToMMSS(runner.runner_results[0].time_seconds)}</Typography>
-                    <Typography sx={{color:'primary.main'}}>{`+${parseSecondsToMMSS(runner.runner_results[0].time_behind.toString())}`}</Typography>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: "space-between",
+                      width: '100%'
+                    }}
+                  >
+                    <Box sx={{
+                      display:'inline-flex',
+                      flexDirection:'row',
+                      justifyContent:'space-between',
+                      gap:'.1em'
+                    }}>
+                      <Typography sx={{color:'secondary.main'}}>{`${t('ResultsStage.StartTime')}:`}</Typography>
+                      <Typography>{parseStartTime(runner.runner_results[0].start_time)}</Typography>
+                    </Box>
+                    <Box sx={{
+                      display:'inline-flex',
+                      flexDirection:'row',
+                      justifyContent:'flex-start',
+                      gap:'.15em',
+                    }}>
+                      <Typography sx={{color:'secondary.main'}}>{`${t('ResultsStage.FinishTime')}:`}</Typography>
+                      <Typography>{(status==="ok")? (runner.runner_results[0].finish_time != null ? parseSecondsToMMSS(runner.runner_results[0].time_seconds) : "-") : t(`ResultsStage.statusCodes.${status}`) }</Typography>
+                      <Typography sx={{color:'primary.main'}}>
+                        {
+                          ((status==="ok")&&(runner.runner_results[0].finish_time != null))
+                            ?
+                          `+${parseSecondsToMMSS(runner.runner_results[0].time_behind.toString())}`
+                            :
+                            ""
+                        }
+                      </Typography>
+                    </Box>
                   </Box>
                 </Box>
-              </Box>
-            </ResultListItem>
-          ))
+              </ResultListItem>
+            )
+          })
         }
       </ResultListContainer>
     )
