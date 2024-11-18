c93a21efe09d108d1e75636d4df6df28fa7613ee
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-11-18 22:42:29 +0100

    Remove 'Organizer' text form EventDetail until backend gives one

--- src/pages/Results/pages/EventDetail/EventDetail.tsx
+++ src/pages/Results/pages/EventDetail/EventDetail.tsx
@@ -88,7 +88,7 @@ export default function EventDetail() {
           bgcolor: "primary.light",
         }}>
 
-        <Typography style={styles.aligns}>Club organizador</Typography>
+        {/**<Typography style={styles.aligns}>Club organizador</Typography>**/}
         <Typography color={"secondary.main"}
           style={styles.titleEvent}>{detail?.description}</Typography>
         {getDatesOfEvent()}
