4ded1f07865773e9c927478e6a8b97e6f3d69d18
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-11-25 23:00:49 +0100

    Update fallback languages for the four spanish official languages

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,7 +7,14 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    debug: true,
-    fallbackLng: 'en',
+    debug: false,
+    fallbackLng: {
+      'ca':['es'], // catalan
+      'eu':['es'], // basque
+      'gl':['es'], // galician
+      'default':['en']
+    },
+    supportedLngs: ['en', 'es', 'fr'], // Explicitly list supported languages
+    load: 'languageOnly', // Ignore region-specific codes like es-ES
   });
 export default i18n;
