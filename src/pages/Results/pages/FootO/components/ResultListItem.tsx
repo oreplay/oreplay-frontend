7afba6cbcc31af9e06c422770cf8288d5db7962f
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-12-10 20:39:30 +0100

    Add a first draft of the virtual ticket

--- src/pages/Results/pages/FootO/components/ResultListItem.tsx
+++ src/pages/Results/pages/FootO/components/ResultListItem.tsx
@@ -3,14 +3,16 @@ import {Box} from "@mui/material";
 
 interface ResultListItemProps {
   children: React.ReactNode
+  onClick?: () => void
 }
 
-const ResultListItem: React.FC<ResultListItemProps> = ({children,}) => {
+const ResultListItem: React.FC<ResultListItemProps> = ({children,onClick}) => {
   return (
     <Box
       display="flex"
       justifyContent="flex-start"
       sx={{ flexDirection: 'row',borderBottom:'1px solid #f2f2f2',marginY:'0 !important',paddingY:'.8em' }}
+      onClick={onClick}
     >
       {children}
     </Box>
--- src/pages/Results/pages/components/SplitsTicket.tsx
+++ src/pages/Results/pages/components/SplitsTicket.tsx
@@ -1,124 +1,177 @@
-import { Box, Typography, Grid, Divider } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Divider,
+  DialogTitle,
+  IconButton,
+  DialogContent, Dialog
+} from "@mui/material";
 import React from "react";
 import { RunnerModel, SplitModel } from '../../../../shared/EntityTypes.ts'
 import { parseDateOnlyTime, parseSecondsToMMSS } from '../../../../shared/Functions.tsx'
+import CloseIcon from "@mui/icons-material/Close";
 
 interface SplitsTicketProps {
-  runner: RunnerModel;
+  isTicketOpen: boolean
+  runner: RunnerModel|null
+  handleCloseTicket: ()=>void
 }
 
-const SplitsTicket: React.FC<SplitsTicketProps> = ({ runner }) => {
-  const runnerResults = runner.runner_results[0]
+const SplitsTicket: React.FC<SplitsTicketProps> = ({ isTicketOpen,runner,handleCloseTicket }) => {
 
-  return (
-    <Box>
-      <Grid container spacing={1}>
-        <Grid item xs={12} sx={{ mb: 1 }}>
-          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-            {`${runner.first_name} ${runner.last_name}`}
-          </Typography>
+  if (runner) {
+    const runnerResults = runner.runner_results[0]
 
-          <Grid container spacing={1}>
-            <Grid item xs={6} sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography>Club: </Typography>
-              <Typography sx={{ color: 'primary.main', fontSize: '1.1rem', fontWeight: 'bold', ml: 1 }}>
-                {runner.club.short_name}
-              </Typography>
-            </Grid>
+    return (
+      <Dialog
+        open={isTicketOpen}
+        onClose={handleCloseTicket}
+        sx={{
+          '& .MuiDialog-paper': {
+            width: '100%',
+            height: 'calc(100% - 5%)',
+            maxWidth: '430px',
+            maxHeight: 'none',
+          },
+        }}
+      >
+        <DialogTitle
+          sx={{
+            marginBottom: 2,
+          }}
+        >
+          <IconButton
+            aria-label="close"
+            onClick={handleCloseTicket}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          {runner ?
+            (
+              <Box>
+                <Grid container spacing={1}>
+                  <Grid item xs={12} sx={{ mb: 1 }}>
+                    <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                      {`${runner.first_name} ${runner.last_name}`}
+                    </Typography>
 
-            <Grid item xs={6} sx={{ display: 'flex', alignItems: 'center' }}>
-              <Typography sx={{ fontWeight: 'bold' }}>{runner.class.short_name}</Typography>
-            </Grid>
-          </Grid>
+                    <Grid container spacing={1}>
+                      <Grid item xs={6} sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Typography>Club: </Typography>
+                        <Typography sx={{ color: 'primary.main', fontSize: '1.1rem', fontWeight: 'bold', ml: 1 }}>
+                          {runner.club.short_name}
+                        </Typography>
+                      </Grid>
 
-          <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>
-            {`SportIdent: ${runner.sicard}`}
-          </Typography>
-        </Grid>
+                      <Grid item xs={6} sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Typography sx={{ fontWeight: 'bold' }}>{runner.class.short_name}</Typography>
+                      </Grid>
+                    </Grid>
 
-        {runnerResults.start_time && (
-          <Grid item xs={6}>
-            <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>{`Start Time: ${parseDateOnlyTime(runnerResults.start_time)}`}</Typography>
-          </Grid>
-        )}
-        <Grid item xs={6}>
-          <Box sx={{ display: 'inline-flex', alignItems: 'center' }}>
-            <Typography>Finish Time: </Typography>
-            <Typography sx={{ color: 'secondary.main', fontWeight: 'bold', fontSize: '1.1em', ml: 1 }}>
-              {parseSecondsToMMSS(runnerResults.time_seconds)}
-            </Typography>
-          </Box>
-        </Grid>
-        {
-          runnerResults.points_final ? (
-            <>
-              <Grid item xs={6}>
-                <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>{`Points bonus: ${runnerResults.points_bonus}`}</Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>{`Points penalty: ${runnerResults.points_penalty}`}</Typography>
-              </Grid>
-              <Grid item xs={6}>{/* empty */}</Grid>
-              <Grid item xs={6}>
-                <Typography sx={{ color: 'secondary.main', fontWeight: 'bold', fontSize: '1.1em' }}>
-                  {runnerResults.points_final.toString()} points
-                </Typography>
-              </Grid>
-            </>
-          ) : null
-        }
+                    <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>
+                      {`SportIdent: ${runner.sicard}`}
+                    </Typography>
+                  </Grid>
 
-        {/* Headers Row 1 */}
-        <Grid item xs={6} sx={{ mt: 2 }}>
-          <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>Parcial</Typography>
-          <Divider />
-        </Grid>
-        <Grid item xs={6} sx={{ mt: 2 }}>
-          <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>Comparación</Typography>
-          <Divider />
-        </Grid>
-        {/* Headers Row 2 */}
-        <Grid item xs={2}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Control</Typography></Grid>
-        <Grid item xs={2}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Tiempo</Typography></Grid>
-        <Grid item xs={3}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Dif.</Typography></Grid>
-        <Grid item xs={2}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Tiempo</Typography></Grid>
-        <Grid item xs={3}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Dif.</Typography></Grid>
+                  {runnerResults.start_time && (
+                    <Grid item xs={6}>
+                      <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>{`Start Time: ${parseDateOnlyTime(runnerResults.start_time)}`}</Typography>
+                    </Grid>
+                  )}
+                  <Grid item xs={6}>
+                    <Box sx={{ display: 'inline-flex', alignItems: 'center' }}>
+                      <Typography>Finish Time: </Typography>
+                      <Typography sx={{ color: 'secondary.main', fontWeight: 'bold', fontSize: '1.1em', ml: 1 }}>
+                        {parseSecondsToMMSS(runnerResults.time_seconds)}
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  {
+                    runnerResults.points_final ? (
+                      <>
+                        <Grid item xs={6}>
+                          <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>{`Points bonus: ${runnerResults.points_bonus}`}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                          <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>{`Points penalty: ${runnerResults.points_penalty}`}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>{/* empty */}</Grid>
+                        <Grid item xs={6}>
+                          <Typography sx={{ color: 'secondary.main', fontWeight: 'bold', fontSize: '1.1em' }}>
+                            {runnerResults.points_final.toString()} points
+                          </Typography>
+                        </Grid>
+                      </>
+                    ) : null
+                  }
 
-        {/* Times Rows */}
-        {runnerResults.splits.map((split: SplitModel) => (
-          <React.Fragment key={split.id}>
-            <Grid item xs={2}>
-              <Typography>
-                <span>{split.order_number?.toString()}</span>
-                <Typography
-                  component="span"
-                  sx={{ ml: '2px', color: 'text.secondary', fontSize: '0.875rem' }}
-                >
-                  ({split.control.station.toString()})
-                </Typography>
-              </Typography>
-              <Typography>
-                {split.points ? `[${split.points}]` : ''}
-              </Typography>
-            </Grid>
-            <Grid item xs={2}>
-              <Typography>{parseDateOnlyTime(split.reading_time)}</Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography>{`+${Math.floor(Math.random() * 60)}:${Math.floor(Math.random() * 60)} (${Math.floor(Math.random() * 10)})`}</Typography>
-              <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>{`+${60 + Math.floor(Math.random() * 60)}:${Math.floor(Math.random() * 60)} (${Math.floor(Math.random() * 10)})`}</Typography>
-            </Grid>
-            <Grid item xs={2}>
-              <Typography>{parseDateOnlyTime(split.reading_time)}</Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography>{`+${Math.floor(Math.random() * 60)}`}</Typography>
-            </Grid>
-          </React.Fragment>
-        ))}
-      </Grid>
-    </Box>
-  );
+                  {/* Headers Row 1 */}
+                  <Grid item xs={6} sx={{ mt: 2 }}>
+                    <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>Parcial</Typography>
+                    <Divider />
+                  </Grid>
+                  <Grid item xs={6} sx={{ mt: 2 }}>
+                    <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>Comparación</Typography>
+                    <Divider />
+                  </Grid>
+                  {/* Headers Row 2 */}
+                  <Grid item xs={2}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Control</Typography></Grid>
+                  <Grid item xs={2}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Tiempo</Typography></Grid>
+                  <Grid item xs={3}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Dif.</Typography></Grid>
+                  <Grid item xs={2}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Tiempo</Typography></Grid>
+                  <Grid item xs={3}><Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>Dif.</Typography></Grid>
+
+                  {/* Times Rows */}
+                  {runnerResults.splits.map((split: SplitModel) => (
+                    <React.Fragment key={split.id}>
+                      <Grid item xs={2}>
+                        <Typography>
+                          <span>{split.order_number?.toString()}</span>
+                          <Typography
+                            component="span"
+                            sx={{ ml: '2px', color: 'text.secondary', fontSize: '0.875rem' }}
+                          >
+                            ({split.control.station.toString()})
+                          </Typography>
+                        </Typography>
+                        <Typography>
+                          {split.points ? `[${split.points}]` : ''}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography>{parseDateOnlyTime(split.reading_time)}</Typography>
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Typography>{`+${Math.floor(Math.random() * 60)}:${Math.floor(Math.random() * 60)} (${Math.floor(Math.random() * 10)})`}</Typography>
+                        <Typography sx={{ color: 'text.secondary', fontSize: '0.875rem' }}>{`+${60 + Math.floor(Math.random() * 60)}:${Math.floor(Math.random() * 60)} (${Math.floor(Math.random() * 10)})`}</Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography>{parseDateOnlyTime(split.reading_time)}</Typography>
+                      </Grid>
+                      <Grid item xs={3}>
+                        <Typography>{`+${Math.floor(Math.random() * 60)}`}</Typography>
+                      </Grid>
+                    </React.Fragment>
+                  ))}
+                </Grid>
+              </Box>
+            )
+            : ""}
+        </DialogContent>
+      </Dialog>
+    );
+  } else {
+    return <></>
+  }
 };
 
 export default SplitsTicket;
