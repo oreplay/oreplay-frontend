71337be20ffd74b8019b4212f2e3376b17f617a4
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-12-13 22:39:55 +0100

    Add some dashes to missing punches in virtual ticket

--- src/pages/Results/pages/components/SplitsTicket.tsx
+++ src/pages/Results/pages/components/SplitsTicket.tsx
@@ -133,10 +133,10 @@ const SplitsTicket: React.FC<SplitsTicketProps> = ({ isTicketOpen,runner,handleC
                         </Typography>
                       </Grid>
                       <Grid item xs={2}>
-                        <Typography>{split.time ? `${parseSecondsToMMSS(split.time)}` : ""}</Typography>
+                        <Typography>{split.time !== null ? `${parseSecondsToMMSS(split.time)}` : "--"}</Typography>
                       </Grid>
                       <Grid item xs={3}>
-                        <Typography>{(split.time_behind === null) ? "" : `+${parseSecondsToMMSS(split.time_behind)}` + ` (${split.position})`}</Typography>
+                        <Typography>{split.time_behind !== null ? `+${parseSecondsToMMSS(split.time_behind)} (${split.position})` : "--"}</Typography>
                       </Grid>
                       <Grid item xs={2}>
                         <Typography>{split.cumulative_time !== null ? `${parseSecondsToMMSS(split.cumulative_time)}` : "--"}</Typography>
