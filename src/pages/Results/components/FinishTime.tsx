2230ef3bd1c5db767e0f158943461ff328837636
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-12-14 13:00:53 +0100

    Create a `<FinishTime />` component

---
+++ src/pages/Results/components/FinishTime.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {RESULT_STATUS_TEXT} from "../shared/constants.ts";
+import {Typography} from "@mui/material";
+import {parseSecondsToMMSS} from "../../../shared/Functions.tsx";
+import {useTranslation} from "react-i18next";
+
+type FinishTimeProps = {
+  status:string|null,
+  finish_time:string|null,
+  time_seconds:number|string|null
+}
+
+const style = {}
+
+const FinishTime: React.FC<FinishTimeProps> = ({status,finish_time,time_seconds}) => {
+  const {t} = useTranslation();
+
+  let parsedStyle = {
+    ...style
+  }
+
+  if(status === RESULT_STATUS_TEXT.ok || status === RESULT_STATUS_TEXT.nc) {
+    console.log("status ok")
+    return (
+      <Typography sx={parsedStyle}>
+        {finish_time != null && time_seconds !== null ? parseSecondsToMMSS(time_seconds) : "-"}
+      </Typography>
+    )
+  } else {
+    switch (status) {
+      case RESULT_STATUS_TEXT.mp:
+        parsedStyle = {
+          ...style,
+          color:'red'
+        }
+        break;
+
+      default:
+        break;
+    }
+    return (
+      <Typography sx={parsedStyle}>
+        {t(`ResultsStage.statusCodes.${status}`)}
+      </Typography>
+    )
+  }
+}
+
+export default FinishTime;
