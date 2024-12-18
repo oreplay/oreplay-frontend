8afd920c189468c79f30871b92cc32530298b2ea
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-12-18 19:33:30 +0100

    REFACTOR: Move `QueryClientProvider` and `AuthProvider` into `main.tsx`

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './i18n.ts';
 import App from "./App.tsx";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {AuthProvider} from "./shared/AuthProvider.tsx";
+
+// Make queries though TanStack Query
+const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
