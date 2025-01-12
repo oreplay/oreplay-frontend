430d7440f6349eaa76051a16bfc18b0bc9fe2028
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-01-12 13:17:16 +0100

    Fix `<NotFoundPage>` layout

--- src/pages/NotFoundError/NotFoundPage.tsx
+++ src/pages/NotFoundError/NotFoundPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Typography, Button, Box } from "@mui/material"
+import { Typography, Button, Box } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import { useTranslation } from "react-i18next"
 import ChangeHistoryIcon from "@mui/icons-material/ChangeHistory"
@@ -12,36 +12,34 @@ const NotFoundPage = () => {
   }
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        textAlign: "center",
+      }}
+    >
+      <Typography variant="h3" component="h1" gutterBottom>
+        {t("404Page.Header")}
+      </Typography>
+      <Typography variant="h5" paragraph>
+        {t("404Page.Message")}
+      </Typography>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={handleHomeClick}
+        startIcon={<ChangeHistoryIcon />}
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          textAlign: "center",
+          textTransform: "none",
         }}
       >
-        <Typography variant="h3" component="h1" gutterBottom>
-          {t("404Page.Header")}
-        </Typography>
-        <Typography variant="h5" paragraph>
-          {t("404Page.Message")}
-        </Typography>
-        <Button
-          variant="outlined"
-          color="primary"
-          onClick={handleHomeClick}
-          startIcon={<ChangeHistoryIcon />}
-          sx={{
-            textTransform: "none",
-          }}
-        >
-          {t("404Page.ButtonMessage")}
-        </Button>
-      </Box>
-    </Container>
+        {t("404Page.ButtonMessage")}
+      </Button>
+    </Box>
   )
 }
 
