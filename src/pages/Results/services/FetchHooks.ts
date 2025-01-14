38c0906bfbae7a0632389c1ce794db83b9cf836b
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-01-14 23:02:41 +0100

    ADD: new useFetchHooks to abstract useQuery

---
+++ src/pages/Results/services/FetchHooks.ts
@@ -0,0 +1,33 @@
+import { useQuery } from "react-query"
+import { AxiosError, AxiosResponse } from "axios"
+import { Data, EventDetailModel } from "../../../shared/EntityTypes.ts"
+import { getEventDetail } from "./EventService.ts"
+
+interface EmptyModel {}
+
+//export function useGet<T,D>(function) {
+//  return useQuery<T, D>({})
+//}
+
+function genericRetryHandler(failureCount: number, error: AxiosError): boolean {
+  if (error.response && error.response.status < 500) {
+    return false
+  }
+  return failureCount < 3
+}
+
+export function useFetchEventDetail(id: string) {
+
+  return useQuery<
+    AxiosResponse<Data<EventDetailModel>, EmptyModel>,
+    AxiosError<Data<EventDetailModel>, EmptyModel>
+  >(
+    ["eventDetail", id], // Query key
+    () => getEventDetail(id, undefined), // Query function
+    {
+      enabled: !!id, // Only fetch if id exists
+      refetchOnWindowFocus: false,
+      retry: genericRetryHandler,
+    },
+  )
+}
