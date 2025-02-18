cc67e11fd2453a93ee03b4faf672926bf0017ad5
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-02-18 21:09:14 +0100

    Update VITE and JavaScript versions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,19 @@
-/// <reference types="vite/client" />
+// <reference types="vite/client" />
+
+// Declare types for env variables
+interface ImportMetaEnv {
+  readonly VITE_API_DOMAIN: string
+  readonly VITE_VERSION_NUMBER: string
+  readonly VITE_VERSION_TYPE: string
+  readonly VITE_DESKTOP_CLIENT_VERSION_FALLBACK: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+// Handle svg files
+declare module "*.svg" {
+  const content: string
+  export default content
+}
