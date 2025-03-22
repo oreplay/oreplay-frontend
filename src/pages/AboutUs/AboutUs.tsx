5999527320e1ac880ac572a4117fd21b2c160b4f
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-03-22 10:28:29 +0100

    Fresh start for organizers page

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,33 +1,55 @@
 import { Trans, useTranslation } from "react-i18next"
-import { Box, Container, Link } from "@mui/material"
+import { Box, Container, createTheme, Link, Theme, ThemeProvider, Typography } from "@mui/material"
+import React from "react"
 const VERSION_NUMBER = import.meta.env.VITE_VERSION_NUMBER
 const VERSION_TYPE = import.meta.env.VITE_VERSION_TYPE
 
 export default function AboutUs() {
-  const { t } = useTranslation()
+  const { t } = useTranslation("about-us")
+
+  const aboutUsTheme = (theme: Theme) =>
+    createTheme({
+      ...theme,
+      typography: {
+        h1: {
+          fontSize: 36,
+          fontWeight: 500,
+          marginBottom: "1rem",
+        },
+        h2: {
+          marginTop: "48px",
+          fontWeight: 500,
+          marginBottom: "16px",
+          fontSize: 26,
+        },
+        h3: {
+          fontWeight: 500,
+          marginBottom: "12px",
+          fontSize: 20,
+        },
+        h4: {
+          fontWeight: 500,
+          marginBottom: "10px",
+          fontSize: 20,
+        },
+        body1: {
+          marginTop: "8px",
+          marginBottom: "8px",
+        },
+      },
+    })
+
+  const style = {
+    alert: {
+      my: "1em",
+    },
+  }
 
   return (
-    <Container>
-      <Box sx={{ m: "50px" }}>
-        <h1>{t("AboutUs.WelcomeMsg")}</h1>
-        <img alt="O-Replay logo" src="/logo.svg" width="124px"></img>
-        <Box sx={{ mt: "30px" }}>
-          {t("AboutUs.ProjectDescription")} {`${VERSION_TYPE}) v${VERSION_NUMBER}`}
-        </Box>
-        <Box sx={{ mt: "30px" }}>
-          <Trans i18nKey="AboutUs.DevelopersHelpMessage">
-            We are proud of being an open source project. Please checkout out GitHub page{" "}
-            <Link href="https://github.com/oreplay" target="_blank">
-              github.com/oreplay
-            </Link>{" "}
-            to find guidance on developing with O-Replay or contributing. Also, checkout our{" "}
-            <Link href="https://www.oreplay.es/api/v1/openapi/" target="_blank">
-              public API documentation
-            </Link>
-            .
-          </Trans>
-        </Box>
-      </Box>
-    </Container>
+    <ThemeProvider theme={aboutUsTheme}>
+      <Container>
+        <Typography variant={"h1"}>{t("Header")}</Typography>
+      </Container>
+    </ThemeProvider>
   )
 }
