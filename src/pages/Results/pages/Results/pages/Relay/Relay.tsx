a99cdd31e8552237095dcc7e39eb41ccbe24298c
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-04-26 23:52:52 +0200

    ADD empty pages for RELAY results

--- src/pages/Results/pages/Results/pages/Relay/Relay.tsx
+++ src/pages/Results/pages/Results/pages/Relay/Relay.tsx
@@ -1,14 +1,52 @@
 import RelayResults from "./pages/RelayResults/RelayResults.tsx"
 import StageLayout from "../../components/Layout/StageLayout.tsx"
 import { useFetchClasses } from "../../../../shared/hooks.ts"
+import { BottomNavigationAction } from "@mui/material"
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents"
+import ResultTabs from "../../components/ResultTabs.tsx"
+import { useTranslation } from "react-i18next"
+import { Person } from "@mui/icons-material"
+import { useQuery } from "react-query"
+import { ProcessedRunnerModel } from "../../../../components/VirtualTicket/shared/EntityTypes.ts"
+import { AxiosError } from "axios"
+import { RunnerModel } from "../../../../../../shared/EntityTypes.ts"
+import { useParams } from "react-router-dom"
+import { useCallback } from "react"
+import RelayLegs from "./pages/RelayLegs/RelayLegs.tsx"
+import { getRelayRunnersByClass } from "./services/RelayService.ts"
+
+const menu_options_labels = ["results", "legs"]
 
 export default function Relay() {
+  const { t } = useTranslation()
+
+  // Get stage's and event's ids
+  const { eventId, stageId } = useParams()
+  if (!eventId || !stageId) {
+    throw new Error("Event Id or Stage Id is missing")
+  }
+
   const [activeClass, setActiveClassId, classesList, areClassesLoading, refreshClasses] =
     useFetchClasses()
 
-  const handleRefreshClick = () => {
+  // Query runners
+  const runnersQueryByClasses = useQuery<ProcessedRunnerModel[], AxiosError<RunnerModel[]>>(
+    ["results", "classes", activeClass?.id],
+    () =>
+      activeClass
+        ? getRelayRunnersByClass(eventId, stageId, activeClass.id)
+        : Promise.reject(new Error("No active class")),
+    {
+      enabled: !!activeClass,
+      refetchOnWindowFocus: false,
+    },
+  )
+
+  // Handle re-fetching
+  const handleRefreshClick = useCallback(() => {
     refreshClasses()
-  }
+    void runnersQueryByClasses.refetch()
+  }, [refreshClasses, runnersQueryByClasses])
 
   return (
     <StageLayout
@@ -18,7 +56,25 @@ export default function Relay() {
       activeClass={activeClass}
       areClassesLoading={areClassesLoading}
     >
-      <RelayResults />
+      <ResultTabs
+        defaultMenu={0}
+        menuOptions={[
+          <BottomNavigationAction
+            key={"relayResults"}
+            label={t("StageHeader.Results")}
+            icon={<EmojiEventsIcon />}
+          />,
+          <BottomNavigationAction
+            key={"RogaineScorePointsMenu"}
+            label={t("StageHeader.RelayLegs")}
+            icon={<Person />}
+          />,
+        ]}
+        menuOptionsLabels={menu_options_labels}
+      >
+        <RelayResults runnersQuery={runnersQueryByClasses} activeClass={activeClass} />
+        <RelayLegs runnersQuery={runnersQueryByClasses} activeClass={activeClass} />
+      </ResultTabs>
     </StageLayout>
   )
 }
--- src/pages/Results/pages/Results/pages/Relay/pages/RelayLegs/RelayLegs.tsx
+++ src/pages/Results/pages/Results/pages/Relay/pages/RelayLegs/RelayLegs.tsx
@@ -1,13 +1,13 @@
-import { Alert } from "@mui/material"
-import ConstructionIcon from "@mui/icons-material/Construction"
-import ResultListContainer from "../../../../../../components/ResultsList/ResultListContainer.tsx"
-import { useTranslation } from "react-i18next"
 import { ResultsPageProps } from "../../../../shared/commonProps.ts"
 import { ProcessedRunnerModel } from "../../../../../../components/VirtualTicket/shared/EntityTypes.ts"
 import { AxiosError } from "axios"
 import { RunnerModel } from "../../../../../../../../shared/EntityTypes.ts"
+import { useTranslation } from "react-i18next"
+import ResultListContainer from "../../../../../../components/ResultsList/ResultListContainer.tsx"
+import { Alert } from "@mui/material"
+import ConstructionIcon from "@mui/icons-material/Construction"
 
-export default function RelayResults(
+export default function RelayLegs(
   // @ts-expect-error TS6133: variable is declared but never used
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   props: ResultsPageProps<ProcessedRunnerModel[], AxiosError<RunnerModel[]>>,
