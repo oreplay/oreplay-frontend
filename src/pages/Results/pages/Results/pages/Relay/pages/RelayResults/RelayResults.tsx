875321b40bdc6078c95be7a278dbb240af882f74
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-04-27 01:04:39 +0200

    ADD sketch of relay result for testing

--- src/pages/Results/pages/Results/pages/Relay/pages/RelayResults/RelayResults.tsx
+++ src/pages/Results/pages/Results/pages/Relay/pages/RelayResults/RelayResults.tsx
@@ -1,23 +1,36 @@
-import { Alert } from "@mui/material"
-import ConstructionIcon from "@mui/icons-material/Construction"
 import ResultListContainer from "../../../../../../components/ResultsList/ResultListContainer.tsx"
-import { useTranslation } from "react-i18next"
 import { ResultsPageProps } from "../../../../shared/commonProps.ts"
 import { ProcessedRunnerModel } from "../../../../../../components/VirtualTicket/shared/EntityTypes.ts"
 import { AxiosError } from "axios"
 import { RunnerModel } from "../../../../../../../../shared/EntityTypes.ts"
+import ChooseClassMsg from "../../../../components/ChooseClassMsg.tsx"
+import ResultsListSkeleton from "../../../../../../components/ResultsList/ResultListSkeleton.tsx"
+import GeneralErrorFallback from "../../../../../../../../components/GeneralErrorFallback.tsx"
+import RelayResultItem from "./components/RelayResultItem.tsx"
 
 export default function RelayResults(
-  // @ts-expect-error TS6133: variable is declared but never used
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   props: ResultsPageProps<ProcessedRunnerModel[], AxiosError<RunnerModel[]>>,
 ) {
-  const { t } = useTranslation()
-  return (
-    <ResultListContainer>
-      <Alert icon={<ConstructionIcon />} severity="info">
-        {t("ThisFunctionalityNotImplementedMsg")}
-      </Alert>
-    </ResultListContainer>
-  )
+  const runnersList = props.runnersQuery.data
+
+  // @ts-expect-error TS6133: variable is declared but never used
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleRowClick = (runner: RunnerModel) => {}
+
+  if (!props.activeClass) {
+    return <ChooseClassMsg />
+  }
+  if (props.runnersQuery.isFetching) {
+    return <ResultsListSkeleton />
+  } else if (props.runnersQuery.isError) {
+    return <GeneralErrorFallback />
+  } else {
+    return (
+      <ResultListContainer>
+        {runnersList?.map((runner: ProcessedRunnerModel) => (
+          <RelayResultItem key={runner.id} runner={runner} handleRowClick={handleRowClick} />
+        ))}
+      </ResultListContainer>
+    )
+  }
 }
