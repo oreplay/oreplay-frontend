6c7a405d4d39af2d601645e49c96bb5015f7ef47
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-05-13 22:22:10 +0200

    UPDATE `<ClassSelector />` to be displayed in a dedicated dialog.

--- src/pages/Results/pages/Results/components/Layout/components/ClassSelector.tsx
+++ src/pages/Results/pages/Results/components/Layout/components/ClassSelector.tsx
@@ -1,6 +1,20 @@
-import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material"
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  List,
+  ListItemButton,
+  ListItemText,
+  OutlinedInput,
+} from "@mui/material"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import { useTranslation } from "react-i18next"
 import { ClassModel } from "../../../../../../../shared/EntityTypes.ts"
+import { useState } from "react"
 
 interface ClassSelectorProps {
   activeClass: ClassModel | null
@@ -12,31 +26,55 @@ interface ClassSelectorProps {
 export default function ClassSelector(props: ClassSelectorProps) {
   const { t } = useTranslation()
 
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const handleClassClick = (newClass: ClassModel): void => {
+    props.setActiveClassId(newClass.id)
+    setIsOpen(false)
+  }
+
   return (
     <Box>
-      <FormControl sx={{ minWidth: "10em" }} required>
-        <InputLabel>{t("ResultsStage.Class")}</InputLabel>
-        <Select
-          id="class-selector-Select-Form"
+      <FormControl
+        sx={{
+          maxWidth: 150,
+          cursor: "pointer",
+        }}
+        onClick={() => setIsOpen(true)} // Clicking anywhere opens dialog
+      >
+        <InputLabel shrink={!!props.activeClass}>{t("ResultsStage.Class")}</InputLabel>
+        <OutlinedInput
+          readOnly
+          notched={!!props.activeClass}
+          value={props.activeClass?.short_name || ""}
+          endAdornment={
+            <InputAdornment position="end">
+              <ExpandMoreIcon />
+            </InputAdornment>
+          }
           label={t("ResultsStage.Class")}
-          onChange={(event) => props.setActiveClassId(event.target.value)}
-          value={props.isLoading ? "loading" : props.activeClass ? props.activeClass.id : ""}
-        >
-          {props.isLoading ? (
-            <MenuItem key={"Loading"} value={"loading"}>
-              {t("Loading")}
-            </MenuItem>
-          ) : (
-            props.classesList?.map((class_item) => {
-              return (
-                <MenuItem key={class_item.id} value={class_item.id}>
-                  {class_item.short_name}
-                </MenuItem>
-              )
-            })
-          )}
-        </Select>
+          sx={{
+            pointerEvents: "none", // disable internal input interaction
+          }}
+          inputProps={{
+            tabIndex: -1,
+          }}
+        />
       </FormControl>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} maxWidth={"xs"} fullWidth>
+        <DialogTitle>{t("ResultsStage.Class")}</DialogTitle>
+        <DialogContent>
+          <List>
+            {props.classesList.map((item) => {
+              return (
+                <ListItemButton onClick={() => handleClassClick(item)}>
+                  <ListItemText>{item.short_name}</ListItemText>
+                </ListItemButton>
+              )
+            })}
+          </List>
+        </DialogContent>
+      </Dialog>
     </Box>
   )
 }
