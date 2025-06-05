ccd802b4a6b662072eb89202f1072761168ec5ac
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-06-05 18:38:23 +0200

    UPDATE Total results to at least parse second in this early preview
    Some work has to be done to refactor the types definitions in a simplified more consistent manner

--- src/pages/Results/pages/Results/pages/Totals/pages/RelayResults/components/StageResultItem.tsx
+++ src/pages/Results/pages/Results/pages/Totals/pages/RelayResults/components/StageResultItem.tsx
@@ -2,6 +2,7 @@ import FlexCol from "../../../../../../../components/FlexCol.tsx"
 import { Typography } from "@mui/material"
 import ResultListItem from "../../../../../../../components/ResultsList/ResultListItem.tsx"
 import { ProcessedOverallModel } from "../../../../../../../components/VirtualTicket/shared/EntityTypes.ts"
+import { parseSecondsToMMSS } from "../../../../../../../../../shared/Functions.tsx"
 
 type StageResultItemProps = {
   stage: ProcessedOverallModel
@@ -11,14 +12,16 @@ export default function StageResultItem({ stage }: StageResultItemProps) {
   return (
     <ResultListItem>
       <FlexCol width={"10px"}>
-        <Typography>{`${stage.position}.`}</Typography>
+        <Typography sx={{ fontSize: "small" }}>{`${stage.position}.`}</Typography>
       </FlexCol>
       <FlexCol flexGrow={1}>
-        <Typography>{`Stage ${stage.stage_order}`}</Typography>
+        <Typography sx={{ fontSize: "small" }}>{`Stage ${stage.stage_order}`}</Typography>
       </FlexCol>
       <FlexCol flexGrow={1}>
-        <Typography>{`${stage.time_seconds} secs`}</Typography>
-        <Typography>{`${stage.points_final} pts`}</Typography>
+        <Typography sx={{ fontSize: "small" }}>
+          {stage.time_seconds ? parseSecondsToMMSS(stage.time_seconds) : "null"}
+        </Typography>
+        <Typography sx={{ fontSize: "small" }}>{stage.points_final}</Typography>
       </FlexCol>
     </ResultListItem>
   )
