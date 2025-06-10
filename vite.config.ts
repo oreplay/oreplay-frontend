eeeda88b5ea8f1466308a54b9858db4f226387e5
Adrian <freefri@freefri.es>
2025-06-10 18:00:20 +0200

    add tailwind with proof of concept in StageResultItem and abstraction components for react native compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
     include: ["@emotion/styled", "@mui/material/Tooltip"],
   },
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   server: {
     allowedHosts: [".oreplay.es"],
     port: 8080,
