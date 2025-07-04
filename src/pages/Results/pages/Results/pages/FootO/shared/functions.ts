227a26f24e14008b3a2f890fb89516aab58cca2f
David Durán Hernández <davidagosto2004@gmail.com>
2025-07-04 15:18:09 +0100

    test: add FootO result ordering tests

---
+++ src/pages/Results/pages/Results/pages/FootO/shared/functions.ts
@@ -0,0 +1,7 @@
+import { ProcessedRunnerModel } from "../../../../../components/VirtualTicket/shared/EntityTypes.ts"
+
+export function orderFootRunners(runnerList: ProcessedRunnerModel[]): ProcessedRunnerModel[] {
+  //TODO: fill this function
+
+  return runnerList
+}
