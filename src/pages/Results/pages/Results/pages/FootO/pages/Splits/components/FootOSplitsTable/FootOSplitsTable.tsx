a619163334637cd2a530519109ac58f64a30c85a
David Durán Hernández <davidagosto2004@gmail.com>
2025-07-24 12:50:56 +0100

    UPDATE: remove graph-related functionality

--- src/pages/Results/pages/Results/pages/FootO/pages/Splits/components/FootOSplitsTable/FootOSplitsTable.tsx
+++ src/pages/Results/pages/Results/pages/FootO/pages/Splits/components/FootOSplitsTable/FootOSplitsTable.tsx
@@ -31,6 +31,7 @@ type FootOSplitsTableProps = {
   radiosList: OnlineControlModel[]
   timeLossEnabled?: boolean
   timeLossThreshold?: number
+  timeLossResults?: TimeLossResults | null   // <--- Añadido aquí
   graphsEnabled?: boolean
   selectedRunners?: string[]
   selectedGraphType?: GraphType | null
