0c198732452457854e87892346dab6fe7344aac6
David Durán Hernández <davidagosto2004@gmail.com>
2025-09-17 10:00:58 +0100

    ENHANCE: update `status_code` type in `EntityTypes` to support `number | string | null` and add default `status_code` values in test cases for improved flexibility and coverage

--- src/shared/EntityTypes.ts
+++ src/shared/EntityTypes.ts
@@ -205,7 +205,7 @@ export interface OverallModel {
   upload_type: string
   stage?: StageOrder | null
   position: number | null
-  status_code: number | null
+  status_code: number | string | null
   time_seconds: number | null
   points_final: number | null
   note: string | null
