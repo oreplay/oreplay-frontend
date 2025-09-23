8d285d373edd9169b1551176723a5ca2dbd50f4c
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-09-23 17:58:17 +0200

    ADD additional test to `getOnlineSplits` in runner processing

--- src/pages/Results/pages/Results/pages/FootO/pages/Splits/components/FootOSplitsTable/shared/footOSplitsTableFunctions.test.ts
+++ src/pages/Results/pages/Results/pages/FootO/pages/Splits/components/FootOSplitsTable/shared/footOSplitsTableFunctions.test.ts
@@ -94,9 +94,6 @@ describe("getOnlineSplits", () => {
     ]
 
     const actual = getOnlineSplits(splitList, radiosList, startTime)
-
-    console.log(expectedSplits)
-    console.log(actual)
     expect(actual).toEqual(expectedSplits)
   })
 
@@ -113,7 +110,7 @@ describe("getOnlineSplits", () => {
         order_number: 1,
         time: 60,
         cumulative_time: 60,
-        reading_time: "2025-08-05T9:01:00.000+00:00",
+        reading_time: "2025-08-05T09:01:00.000+00:00",
       },
       {
         ...baseControl,
@@ -126,7 +123,7 @@ describe("getOnlineSplits", () => {
         order_number: 2,
         time: 120,
         cumulative_time: 180,
-        reading_time: "2025-08-05T9:03:00.000+00:00",
+        reading_time: "2025-08-05T09:03:00.000+00:00",
       },
       {
         ...baseControl,
@@ -139,7 +136,7 @@ describe("getOnlineSplits", () => {
         order_number: 3,
         time: 240,
         cumulative_time: 420,
-        reading_time: "2025-08-05T9:07:00.000+00:00",
+        reading_time: "2025-08-05T09:07:00.000+00:00",
       },
       {
         ...baseControl,
@@ -148,7 +145,7 @@ describe("getOnlineSplits", () => {
         order_number: Infinity,
         time: 10,
         cumulative_time: 430,
-        reading_time: "2025-08-05T9:07:10.000+00:00",
+        reading_time: "2025-08-05T09:07:10.000+00:00",
       },
     ]
 
@@ -161,7 +158,220 @@ describe("getOnlineSplits", () => {
     expect(actual).toEqual(expectedSplits)
   })
 
-  it("should get online splits when there is a full chip reading")
+  it("should handle missing online punches", () =>{
 
-  it("should handle missing online punches")
+    const splitList: ProcessedSplitModel[] = [
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-41",
+          station: "41",
+        },
+        id: "41",
+        order_number: 2,
+        time: 120,
+        cumulative_time: 180,
+        reading_time: "2025-08-05T09:03:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: null,
+        id: "finish",
+        order_number: Infinity,
+        time: null,
+        cumulative_time: null,
+        reading_time: null,
+      },
+    ]
+
+    const expectedOnlineSplitList: RadioSplitModel[] = [
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "missingRadio-control-31",
+          station: "31",
+        },
+        id: "missingRadio-31",
+        is_next: null,
+        order_number: 1,
+      },
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-41",
+          station: "41",
+        },
+        id: "41",
+        is_next: null,
+        order_number: 2,
+        time: 120,
+        cumulative_time: 180,
+        reading_time: "2025-08-05T09:03:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "missingRadio-control-51",
+          station: "51",
+        },
+        id: "missingRadio-51",
+        is_next: DateTime.fromISO("2025-08-05T09:03:00.000+00:00"),
+        order_number: 3,
+      },
+      {
+        ...baseControl,
+        control: null,
+        id: "finish",
+        order_number: Infinity,
+        is_next: null,
+      },
+    ]
+
+    const actual = getOnlineSplits(splitList, radiosList, startTime)
+    expect(actual).toEqual(expectedOnlineSplitList)
+
+
+  })
+
+  it("should get online splits when there is a full chip reading", () => {
+    const splitList: ProcessedSplitModel[] = [
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-31",
+          station: "31",
+        },
+        id: "31",
+        order_number: 1,
+        time: 60,
+        cumulative_time: 60,
+        reading_time: "2025-08-05T09:01:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-32",
+          station: "32",
+        },
+        id: "32",
+        order_number: 2,
+        time: 120,
+        cumulative_time: 180,
+        reading_time: "2025-08-05T09:03:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-41",
+          station: "41",
+        },
+        id: "41",
+        order_number: 3,
+        time: 60,
+        cumulative_time: 240,
+        reading_time: "2025-08-05T09:04:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-42",
+          station: "42",
+        },
+        id: "42",
+        order_number: 4,
+        time: 130,
+        cumulative_time: 370,
+        reading_time: "2025-08-05T09:06:10.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-51",
+          station: "51",
+        },
+        id: "51",
+        order_number: 5,
+        time: 50,
+        cumulative_time: 420,
+        reading_time: "2025-08-05T09:07:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: null,
+        id: "finish",
+        order_number: Infinity,
+        time: 10,
+        cumulative_time: 440,
+        reading_time: "2025-08-05T09:07:10.000+00:00",
+      },
+    ]
+
+    const expectedOnlineControls: RadioSplitModel[] = [
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-31",
+          station: "31",
+        },
+        id: "31",
+        is_next: null,
+        order_number: 1,
+        time: 60,
+        cumulative_time: 60,
+        reading_time: "2025-08-05T09:01:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-41",
+          station: "41",
+        },
+        id: "41",
+        is_next: null,
+        order_number: 3,
+        time: 60,
+        cumulative_time: 240,
+        reading_time: "2025-08-05T09:04:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: {
+          ...baseControl.control,
+          id: "control-51",
+          station: "51",
+        },
+        id: "51",
+        is_next: null,
+        order_number: 5,
+        time: 50,
+        cumulative_time: 420,
+        reading_time: "2025-08-05T09:07:00.000+00:00",
+      },
+      {
+        ...baseControl,
+        control: null,
+        id: "finish",
+        is_next: null,
+        order_number: Infinity,
+        time: 10,
+        cumulative_time: 440,
+        reading_time: "2025-08-05T09:07:10.000+00:00",
+      },
+    ]
+
+    const actual = getOnlineSplits(splitList, radiosList, startTime)
+
+    expect(actual).toEqual(expectedOnlineControls)
+  })
 })
