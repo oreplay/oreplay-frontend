f2abcbd456830ef9511392cdfa0f446652c27d65
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2025-10-07 20:34:45 +0200

    ADD 'Counted' and 'Dropped' chips to `<StageResultItem />`

---
+++ src/pages/Results/pages/Results/pages/Totals/pages/RelayResults/components/StageResultItem/components/Chips/ContributoryChip.tsx
@@ -0,0 +1,22 @@
+import { Chip } from "@mui/material"
+import { useTranslation } from "react-i18next"
+
+export default function ContributoryChip() {
+  const { t } = useTranslation()
+
+  return (
+    <Chip
+      label={t("ResultsStage.Contributory")}
+      variant={"outlined"}
+      color={"success"}
+      sx={{
+        fontSize: "0.65rem",
+        height: 18,
+        "& .MuiChip-label": {
+          px: 0.6,
+          py: 0,
+        },
+      }}
+    />
+  )
+}
---
+++ src/pages/Results/pages/Results/pages/Totals/pages/RelayResults/components/StageResultItem/components/Chips/NonContributoryChip.tsx
@@ -0,0 +1,22 @@
+import { Chip } from "@mui/material"
+import { useTranslation } from "react-i18next"
+
+export default function NonContributoryChip() {
+  const { t } = useTranslation()
+
+  return (
+    <Chip
+      label={t("ResultsStage.NonContributory")}
+      variant={"outlined"}
+      color={"warning"}
+      sx={{
+        fontSize: "0.65rem",
+        height: 18,
+        "& .MuiChip-label": {
+          px: 0.6,
+          py: 0,
+        },
+      }}
+    />
+  )
+}
