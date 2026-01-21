35751be92c6af42c98720715859c89628c6e8b5d
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2026-01-21 23:13:56 +0100

    ADD a brand-new sign-up

---
+++ src/pages/Administration/shared/EntityTypes.ts
@@ -0,0 +1,16 @@
+import { Data } from "../../../shared/EntityTypes.ts"
+
+export type SignUpApiResponse = Data<SignUpResponseModel>
+
+interface SignUpResponseModel extends SignUpUser {
+  id: string
+  created: string
+  modified: string
+}
+
+export interface SignUpUser {
+  first_name: string
+  last_name: string
+  email: string
+  password: string
+}
