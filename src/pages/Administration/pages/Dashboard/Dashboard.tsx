a00569063027e438e12e9170e6a897d77c0d253b
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2026-02-19 22:10:25 +0100

    BUG FIX: missing translation key

--- src/pages/Administration/pages/Dashboard/Dashboard.tsx
+++ src/pages/Administration/pages/Dashboard/Dashboard.tsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
     <Box sx={{ minHeight: "100vh", flexGrow: 1, backgroundColor: "#f6f6f6", py: 6 }}>
       <Container maxWidth={"md"}>
         <Typography component="h1" variant="h5" fontWeight={600} gutterBottom>
-          {t("Dashboard.YourEvents")}
+          {t("Dashboard.YourEvents.title")}
         </Typography>
         <Typography component={"p"} variant={"body2"} color="text.secondary" sx={{ mb: 4 }}>
           {t("Dashboard.YourEvents.description")}
