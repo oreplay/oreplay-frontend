8f24bfbbfc6e0869991339e59233e581ae07318f
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2026-03-18 23:29:30 +0100

    Make stage's start time be local according to event's timezone

--- src/pages/Administration/pages/EventAdmin/pages/EventAdmin/components/StagesDataGrid/components/GridDateTimeEditCell/index.tsx
+++ src/pages/Administration/pages/EventAdmin/pages/EventAdmin/components/StagesDataGrid/components/GridDateTimeEditCell/index.tsx
@@ -3,18 +3,26 @@ import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker"
 import { GridRenderEditCellParams, useGridApiContext } from "@mui/x-data-grid"
 import { StageRow } from "../../index.tsx"
 import { renderTimeViewClock } from "@mui/x-date-pickers"
+import { TimeZoneId } from "../../../../../../components/EventAdminForm/components/TimeZoneAutocomplete"
 
-export function GridLuxonDateTimeEditCell(
-  props: GridRenderEditCellParams<StageRow, DateTime | null, string>,
-) {
-  const { id, field, value } = props
+interface GridLuxonDateTimeEditCellProps
+  extends GridRenderEditCellParams<StageRow, DateTime | null, string> {
+  timezone: TimeZoneId
+}
+
+export function GridLuxonDateTimeEditCell(props: GridLuxonDateTimeEditCellProps) {
+  const { id, field, value, timezone } = props
   const apiRef = useGridApiContext()
 
   const handleChange = (newValue: DateTime | null) => {
+    // Set timezone
+    const zoned = newValue?.setZone(timezone, { keepLocalTime: false }) // use event's timezone
+    console.log("Timezone for editing zone", timezone)
+    // change value on row
     void apiRef.current.setEditCellValue({
       id,
       field,
-      value: newValue,
+      value: zoned,
     })
   }
 
@@ -22,6 +30,7 @@ export function GridLuxonDateTimeEditCell(
     <DateTimePicker
       value={value ?? null}
       onChange={handleChange}
+      timezone={timezone} // use event's timezone
       slotProps={{
         textField: {
           variant: "outlined",
