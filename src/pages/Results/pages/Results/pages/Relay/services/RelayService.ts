c81d049fe71cbe2ab32f6f1a8fe87ab10855ff04
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2026-04-01 15:11:59 +0200

    BUG FIX: `getRelayRunnersByClass(...)` and `getRelayRunnersByClub(...)` returned runners in the wrong order (according to `sortRelayRunners(...)`

--- src/pages/Results/pages/Results/pages/Relay/services/RelayService.ts
+++ src/pages/Results/pages/Results/pages/Relay/services/RelayService.ts
@@ -2,6 +2,7 @@ import { ProcessedRunnerModel } from "../../../../../components/VirtualTicket/sh
 import { getRunnersInStage } from "../../../../../services/EventService.ts"
 import { sortRunners } from "../../../../../shared/sortingFunctions/sortRunners.ts"
 import { processRunnerData } from "../../../../../components/VirtualTicket/shared/virtualTicketFunctions.ts"
+import { sortRelayRunners } from "../shared/sortRelayRunners.ts"
 
 export async function getRelayRunnersByClass(
   eventId: string,
@@ -16,7 +17,10 @@ export async function getRelayRunnersByClass(
   runnersList = sortRunners(runnersList)
 
   // Runner processing
-  return processRunnerData(runnersList)
+  const processRunners = processRunnerData(runnersList)
+
+  // Final sort
+  return sortRelayRunners(processRunners)
 }
 
 export async function getRelayRunnersByClub(
@@ -32,5 +36,8 @@ export async function getRelayRunnersByClub(
   runnersList = sortRunners(runnersList)
 
   // Runner processing
-  return processRunnerData(runnersList)
+  const processRunners = processRunnerData(runnersList)
+
+  // Final sort
+  return sortRelayRunners(processRunners)
 }
