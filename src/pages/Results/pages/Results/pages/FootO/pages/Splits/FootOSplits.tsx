a9146076a9f46ecc90221b7c08b0719b520e7215
Andreu Espinosa Bajo <udernaeb@gmail.com>
2026-09-11 17:36:27 +0200

    REMOVE unused timeLossResults from FootOSplits and FootOSplitsTable components

--- src/pages/Results/pages/Results/pages/FootO/pages/Splits/FootOSplits.tsx
+++ src/pages/Results/pages/Results/pages/FootO/pages/Splits/FootOSplits.tsx
@@ -108,11 +108,6 @@ export default function FootOSplits(
     }
   }
 
-  const timeLossResults: TimeLossResults | undefined = useMemo(() => {
-    if (!timeLossThreshold) return undefined
-    return analyzeTimeLoss(runners, timeLossThreshold)
-  }, [runners, timeLossThreshold])
-
   const barChartTimeLossResults: TimeLossResults | undefined = useMemo(() => {
     if (!barChartThreshold) return undefined
     return analyzeTimeLoss(runners, barChartThreshold)
@@ -160,7 +155,6 @@ export default function FootOSplits(
           runners={runners}
           timeLossEnabled={false}
           timeLossThreshold={timeLossThreshold}
-          timeLossResults={undefined}
         />
       </Box>
     )
@@ -179,7 +173,6 @@ export default function FootOSplits(
           runners={runners}
           timeLossEnabled={false}
           timeLossThreshold={timeLossThreshold}
-          timeLossResults={undefined}
         />
       </Box>
     )
@@ -201,7 +194,6 @@ export default function FootOSplits(
           runners={runners}
           timeLossEnabled={false}
           timeLossThreshold={timeLossThreshold}
-          timeLossResults={undefined}
         />
       </Box>
     )
@@ -243,7 +235,6 @@ export default function FootOSplits(
           runners={runners}
           timeLossEnabled={true}
           timeLossThreshold={timeLossThreshold}
-          timeLossResults={timeLossResults}
         />
       </Box>
     )
