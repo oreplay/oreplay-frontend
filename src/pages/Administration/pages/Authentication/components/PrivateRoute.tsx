c2b2f0b06f31a6edb5242d9f7a2bf9a3dd02d148
Adrian <freefri@freefri.es>
2026-09-16 18:10:17 +0200

    fix: return to previous location after a page reload from the under login section
    e.g. from /admin/id reload the page should take you back to the same page

--- src/pages/Administration/pages/Authentication/components/PrivateRoute.tsx
+++ src/pages/Administration/pages/Authentication/components/PrivateRoute.tsx
@@ -1,8 +1,10 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Navigate, Outlet, useLocation } from "react-router-dom"
 import { useAuth } from "../../../../../shared/hooks.ts"
+import { signInPath } from "../shared/signInRedirect.ts"
 
 export default function PrivateRoute() {
   const { user } = useAuth()
+  const location = useLocation()
 
-  return user ? <Outlet /> : <Navigate to={`/signin`} />
+  return user ? <Outlet /> : <Navigate to={signInPath(location)} />
 }
--- src/pages/Administration/pages/Authentication/pages/InItSignIn.tsx
+++ src/pages/Administration/pages/Authentication/pages/InItSignIn.tsx
@@ -1,13 +1,19 @@
-import { getSignInUrl } from "../../../services/AuthenticationService.ts"
+import { getSignInUrl, storeLoginRedirectPath } from "../../../services/AuthenticationService.ts"
 import { useEffect } from "react"
+import { useSearchParams } from "react-router-dom"
 import GeneralSuspenseFallback from "../../../../../components/GeneralSuspenseFallback.tsx"
+import { REDIRECT_PARAM } from "../shared/signInRedirect.ts"
 
 export default function InItSignIn() {
+  const [searchParams] = useSearchParams()
+  const redirectPath = searchParams.get(REDIRECT_PARAM)
+
   useEffect(() => {
+    storeLoginRedirectPath(redirectPath)
     void getSignInUrl().then((url: string) => {
       window.location.replace(url)
     })
-  }, [])
+  }, [redirectPath])
 
   return <GeneralSuspenseFallback />
 }
